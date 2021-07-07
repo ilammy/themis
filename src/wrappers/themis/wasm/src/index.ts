843a294945fd174dff49d353bdaf9cdb0408bf64
Alexei Lozovsky <me@ilammy.net>
2021-07-07 20:47:16 +0900

    [hax] initialized

--- src/wrappers/themis/wasm/src/index.ts
+++ src/wrappers/themis/wasm/src/index.ts
@@ -28,9 +28,26 @@ export { SecureSession } from "./secure_session";
 export { KeyPair, PrivateKey, PublicKey, SymmetricKey } from "./secure_keygen";
 export { SecureComparator } from "./secure_comparator";
 
+// WebAssembly code is not directly included with compiled JavaScript code.
+// Emscripten generates a stub that will download and compile WebAssembly
+// after this module is loaded. This initialization is necessary before
+// any WasmThemis function can be called.
+//
+// Due to historical reasons, WasmThemis has multiple ways to initialize.
+
+let onRuntimeInitialized: (value: void | PromiseLike<void>) => void
+
+// The current facility is "async initialize()" function which user code
+// is expected to call, then await the result to resolve before using other
+// WasmThemis functions.
+//
+// Calling initialize() kicks off the download and compilation process.
+// This also allows to pass some extra parameters to Emscripten stub,
+// such as the URL from where "libthemis.wasm" is to be obtained.
+
 export const initialize = async (wasmPath?: string) => {
   context.libthemis = await libthemisFn({
-    onRuntimeInitialized: function () {},
+    onRuntimeInitialized: () => onRuntimeInitialized(),
     locateFile: wasmPath ? function () {
       return wasmPath;
     } : undefined,
@@ -38,3 +55,72 @@ export const initialize = async (wasmPath?: string) => {
 
   return context.libthemis;
 };
+
+// However, it was not always the case. Previously, WasmThemis has exported
+// just "initialzed" promise which is resolved once WASM code is downloaded
+// and compiled. User code is expected to await for that promise to resolve,
+// then proceed using WasmThemis functions.
+//
+// Back in the day, WasmThemis was not modularized, so the download & compile
+// was initialized immediately once JS code of WasmThemis got loaded and the
+// module was evalated.
+//
+// Obviously, there is no way to pass any parameters to this promise either.
+//
+// In order to keep "initialized" working, we do some trickery, exporting
+// a promise that will make sure that initialize() is called before this
+// promise is resolved. This kickstarts WebAssmebly loading and ensures
+// that WasmThemis is initialized once "initialized" promise is resolved.
+//
+// This approach does not cover the case where the user code does not use
+// "initialized" promise at all. That is, if the application just hopes
+// that WasmThemis is loaded by the time it's needed. This should be rare.
+// Users have been warned.
+
+// TypeScript does not allow to extend Promise nicely, but since JavaScript
+// is actually duck-typed, we can just mimick the API and get away with it.
+// https://github.com/microsoft/TypeScript/issues/15202
+class InitializedPromise {
+  private initialized: boolean = false;
+  private readonly promise: Promise<void>;
+
+  constructor(executor: (resolve: (value: void | PromiseLike<void>) => void,
+                         reject: (reason?: any) => void) => void)
+  {
+    this.promise = new Promise(executor);
+  }
+
+  private initialize() {
+    // Make sure that initialize() -- the exported one -- is only called once.
+    // Promises can have their then() and catch() methods called multiple times
+    // to register multiple callbacks. Register callbacks, but call initialize()
+    // only once. Also, it's okay to ignore its result. For the "initialized"
+    // code path we register "onRuntimeInitialized" callback that will resolve
+    // *this* promise when it is time.
+    if (!this.initialized) {
+      initialize();
+      this.initialized = true;
+    }
+  }
+
+  then<T = any, E = never>(
+    fulfilled?: ((value: any) => T | PromiseLike<T>) | null | undefined,
+    rejected?: ((reason: any) => E | PromiseLike<E>) | null | undefined,
+  ) : Promise<T | E>
+  {
+    this.initialize();
+    return this.promise.then(fulfilled, rejected);
+  }
+
+  catch<T = never>(
+    rejected?: ((reason: any) => T | PromiseLike<T>) | undefined | null,
+  ): Promise<void | T>
+  {
+    this.initialize();
+    return this.promise.catch(rejected);
+  }
+}
+
+export const initialized = new InitializedPromise((resolve) => {
+  onRuntimeInitialized = resolve;
+});
